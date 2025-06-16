be4de749db2bbaefcd8eac1e2e7b0cc8fb97b352
zako meshvildishvili <zaurimeshvildishvili@gmial.com>
2025-06-16 18:27:02 +0400

    cd

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ commands = {
     "echo": lambda *args: print(" ".join(args)),
     "exit":lambda exit_code:sys.exit(int(exit_code[0])) if exit_code else 0,
     "pwd": lambda: print(os.getcwd()),
+    "cd":lambda path: os.chdir(path),
     "type":lambda *command:type(command),
     
 }
