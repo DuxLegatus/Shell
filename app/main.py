4f542f6d9c648a339ee289622701f294123fb07a
zako meshvildishvili <zaurimeshvildishvili@gmial.com>
2025-06-16 14:18:29 +0400

    exec

--- app/main.py
+++ app/main.py
@@ -4,9 +4,18 @@ import shutil
 commands = {
     "echo": lambda args: print(" ".join(args)),
     "exit":lambda exit_code:sys.exit(int(exit_code[0])) if exit_code else 0,
-    "type":lambda command:print(f"{command[0]} is {shutil.which(command[0])}")    
-    if shutil.which(command[0]) else print(f"{command[0]}: not found")
+    "type":lambda command:type(command)
 }
+def type(command):
+    if command[0] in commands:
+        print(f"{command[0]} is a shell builtin")    
+    elif shutil.which(command[0]):
+        print(f"{command[0]}: is {shutil.which(command[0])}")
+    else:
+        print(f"{command[0]}: not found")
+
+
+
 def main():
     # Uncomment this block to pass the first stage
     while True:
