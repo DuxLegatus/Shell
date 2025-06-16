9950f8bfba0892308492c9bb576723cd1627fbd9
zako meshvildishvili <zaurimeshvildishvili@gmial.com>
2025-06-16 13:14:36 +0400

    Exit

--- app/main.py
+++ app/main.py
@@ -6,8 +6,9 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
+        if command == "exit 0":
+            sys.exit()
         print(f"{command}: command not found")
-        sys.exit("exit 0")
 
 
     # Wait for user input
